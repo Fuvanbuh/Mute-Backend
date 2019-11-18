6d5707a58d1b4cedabcf9053370bee33bcc4071c
noemi alonso <noemialonso@MacBook-Pro-de-noemi.local>
2019-11-18 21:29:28 +0100

    fixed route to findOneStory

--- routes/story.js
+++ routes/story.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { isLoggedIn } = require('../helpers/middlewares');
+const {
+  isLoggedIn
+} = require('../helpers/middlewares');
 
 const Map = require("../models/Map");
 const Story = require('../models/Story');
@@ -16,6 +18,17 @@ router.get('/', isLoggedIn(), async (req, res, next) => {
   }
 });
 
+router.get('/:storyId', async (req, res, next) => {
+  const {
+    storyId
+  } = req.params;
+  try {
+    const story = await Story.findById(storyId);
+    res.status(200).json(story);
+  } catch (error) {
+    next(error);
+  }
+});
 router.get('/themes', isLoggedIn(), async (req, res, next) => {
   try {
     const themes = await Theme.find();
@@ -30,7 +43,7 @@ router.get('/themes', isLoggedIn(), async (req, res, next) => {
 router.post('/addStory', isLoggedIn(), async (req, res, next) => {
   try {
     const userId = req.session.currentUser._id;
-  
+
     const newStory = req.body;
     newStory.creator = userId;
     const createdStory = await Story.create(newStory);
@@ -42,13 +55,16 @@ router.post('/addStory', isLoggedIn(), async (req, res, next) => {
 
 //edit story
 router.put('/:idStory/edit', isLoggedIn(), async (req, res, next) => {
-  const { idStory } = req.params;
+  const {
+    idStory
+  } = req.params;
   const storyUpdated = req.body;
   try {
     const updated = await Story.findByIdAndUpdate(
       idStory,
-      storyUpdated,
-      { new: true }
+      storyUpdated, {
+        new: true
+      }
     );
 
     res.status(200).json(updated);
